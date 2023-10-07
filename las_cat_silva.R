8847590af621616882a2f7a4a5a0ef4df205d3b0
Marius Tsui <kt255@students.waikato.ac.nz>
2023-10-08 06:53:44 +1300

    Get the min height from 5M and filter out by tree height to get different bush density

--- las_cat_silva.R
+++ las_cat_silva.R
@@ -8,20 +8,28 @@ remove(list = ls())
 gc()
 
 # Set the minimum height of a tree to be detected ( in meters )
-hmin = 20
+
+hmin <- c(5)
+loop_index <- 0
+forest_metrics <- matrix(nrow = 6, ncol=4)
+colnames(forest_metrics) <- c("Number of Trees", "Trees per hectare", "Crown area(m2)", "Crown Cover")
+rownames(forest_metrics) <- c("5M", "10M", "15M", "20M", "30M", "40M")
 
 # read all the point cloud (.las) into a lasCatalog
+  
+
   las_catg <- readLAScatalog("D:\\PROJECT_TREES\\REQ201107_Lars\\laser\\", filter="-drop_classification 6 7 18")
   #las_catg <- readLAScatalog("D:\\PROJECT_TREES\\LAS\\", filter="-drop_classification 6 7 18")
+  
+  
   las_catg
   las_check(las_catg)
 
 # Locate Trees from LAS
   # function to define the window size for locate_trees
   getWS <- function(x){
-    y <- 2.6125 + x^0.665
+    y <- 1.6125 + x^0.665
     y[x<2] <- 2
-    y[x>40] <- 15
     return(y)
   }
   
@@ -29,39 +37,70 @@ hmin = 20
   fill.na <- function(x, i=5) { if (is.na(x)[i]) { return(mean(x, na.rm = TRUE)) } else { return(x[i]) }}
   w <- matrix(1, 3, 3)
   
-  f_treecrown <- function(las){
+  f_treecrown <- function(las, hmin){
     
     las <- classify_noise(las, sor(15,7))
     las_denoise <- filter_poi(las, Classification != LASNOISE)
     nlas <- normalize_height(las_denoise, knnidw())
     tree_tops <- locate_trees(nlas, lmf(ws = getWS, hmin = hmin))
-    chm <- rasterize_canopy(nlas, 0.3, pitfree(subcircle = 0.2))
+    chm <- rasterize_canopy(nlas, 1, pitfree(subcircle = 0.2))
     smooth_chm <- terra::focal(chm, w, fun = mean, na.rm = TRUE)
     tree <- segment_trees(nlas, silva2016(smooth_chm, tree_tops, max_cr_factor = 0.3, exclusion = 0.25, ID="treeID"))
     crowns <- crown_metrics(  tree,  type = "convex",  func = .stdtreemetrics, attribute = "treeID", geom = "convex")
+    diff <- Sys.time() - start
+    print(paste0("Lapsed for ", format(round(diff, 2), nsmall = 2)))
     return(crowns)
   }
   
-  opt    <- list(automerge = TRUE)   # catalog_apply will merge the outputs into a single object
-  output = NULL
-  output <- catalog_map(las_catg, f_treecrown, .options = opt)
-  names(output) <- basename(las_catg$filename)
-  
-  lapply( seq_along(output), function(i){
-    single_sf <- dplyr::bind_rows(output[i])
+  start <- Sys.time()
+  print(paste0("Project started at ", start))
+  for (h in hmin){
+    loop_index <- loop_index + 1
+    opt    <- list(automerge = TRUE)   # catalog_apply will merge the outputs into a single object
+    output = NULL
+    output <- catalog_map(las_catg, f_treecrown, hmin = h, .options = opt)
+    
+    single_sf <- dplyr::bind_rows(output)
     types <- st_geometry_type(single_sf)
     types_df <- data.frame(types)
     my_labelled_sf_object <- merge(single_sf, types_df,by.x=0, by.y=0, all.x=TRUE)
     my_filtered_sf_object <- my_labelled_sf_object[my_labelled_sf_object$types == "POLYGON",]
-    st_write(my_filtered_sf_object, paste0("D:\\PROJECT_TREES\\R_OUTPUT\\CROWNS\\",substring(names(output)[[i]], 1, 23),'_',hmin,'PLUS.shp' ), append=FALSE)
-  })
+    
+    
+    st_write(my_filtered_sf_object, paste0("D:\\PROJECT_TREES\\R_OUTPUT\\CROWNS",'_',h,'PLUS_SILVA.shp' ), append=FALSE)
+    #st_write(my_filtered_sf_object, paste0("D:\\PROJECT_TREES\\isotree_trail\\ALLCROWNS",'_',h,'PLUS_SILVA.shp' ), append=FALSE)
+    
+    
+    a_ha <- units::set_units(st_area(las_catg), ha)
+    a_m2 <- units::set_units(st_area(las_catg), m^2)
+    tph <- nrow(my_filtered_sf_object) / (as.numeric(a_ha))
+    cc <- sum(my_filtered_sf_object$convhull_area) / (as.numeric(a_m2))
+    forest_metrics[loop_index, 1] <- nrow(my_filtered_sf_object)
+    forest_metrics[loop_index, 2] <- round(tph, 2)
+    forest_metrics[loop_index, 3] <- sum(my_filtered_sf_object$convhull_area)
+    forest_metrics[loop_index, 4] <- round(cc, 2)
+  }
   
-  single_sf <- dplyr::bind_rows(output)
-  types <- st_geometry_type(single_sf)
-  types_df <- data.frame(types)
-  my_labelled_sf_object <- merge(single_sf, types_df,by.x=0, by.y=0, all.x=TRUE)
-  my_filtered_sf_object <- my_labelled_sf_object[my_labelled_sf_object$types == "POLYGON",]
-  st_write(my_filtered_sf_object, paste0("D:\\PROJECT_TREES\\R_OUTPUT\\CROWNS",'_',hmin,'PLUS_SILVA.shp' ), append=FALSE)
+  df <- as.data.frame((forest_metrics))
   
   
+  tree_5 <- my_filtered_sf_object
+  tree_10 <- tree_5[tree_5$Z >= 10,]
+  st_write(tree_10, paste0("D:\\PROJECT_TREES\\R_OUTPUT\\CROWNS",'_10','PLUS_SILVA.shp' ), append=FALSE)
+  df[2, ] <- c(nrow(tree_10), round(nrow(tree_10)/ as.numeric(a_ha), 2), round(sum(tree_10$convhull_area), 2), round( (sum(tree_10$convhull_area)/as.numeric(a_m2)*100), 2))
+  tree_15 <- tree_5[tree_5$Z >= 15,]
+  st_write(tree_15, paste0("D:\\PROJECT_TREES\\R_OUTPUT\\CROWNS",'_15','PLUS_SILVA.shp' ), append=FALSE)
+  df[3, ] <- c(nrow(tree_15), round(nrow(tree_15)/ as.numeric(a_ha), 2), round(sum(tree_15$convhull_area), 2), round( (sum(tree_15$convhull_area)/as.numeric(a_m2)*100), 2))
+  tree_20 <- tree_5[tree_5$Z >= 20,]
+  st_write(tree_20, paste0("D:\\PROJECT_TREES\\R_OUTPUT\\CROWNS",'_20','PLUS_SILVA.shp' ), append=FALSE)
+  df[4, ] <- c(nrow(tree_20), round(nrow(tree_20)/ as.numeric(a_ha), 2), round(sum(tree_20$convhull_area), 2), round( (sum(tree_20$convhull_area)/as.numeric(a_m2)*100), 2))
+  tree_30 <- tree_5[tree_5$Z >= 30,]
+  st_write(tree_30, paste0("D:\\PROJECT_TREES\\R_OUTPUT\\CROWNS",'_30','PLUS_SILVA.shp' ), append=FALSE)
+  df[5, ] <- c(nrow(tree_30), round(nrow(tree_30)/ as.numeric(a_ha), 2), round(sum(tree_30$convhull_area), 2), round( (sum(tree_30$convhull_area)/as.numeric(a_m2)*100), 2))
+  tree_40 <- tree_5[tree_5$Z >= 40,]
+  st_write(tree_40, paste0("D:\\PROJECT_TREES\\R_OUTPUT\\CROWNS",'_40','PLUS_SILVA.shp' ), append=FALSE)
+  df[6, ] <- c(nrow(tree_40), round(nrow(tree_40)/ as.numeric(a_ha), 2), round(sum(tree_40$convhull_area), 2), round( (sum(tree_40$convhull_area)/as.numeric(a_m2)*100), 2))
+  
+  write.csv(df, "D:\\PROJECT_TREES\\R_OUTPUT\\FOREST_METRICS.csv")
+  #write.csv(df, "D:\\PROJECT_TREES\\isotree_trail\\FOREST_METRICS.csv")
   
--- script.R
+++ script.R
@@ -6,7 +6,7 @@ library("dplyr")
 remove(list = ls())
 
 col <- height.colors(50)
-ori_las <- readLAS("D:\\PROJECT_TREES\\REQ201107_Lars\\laser\\CL2_BE33_2021_1000_0504.las", filter="-drop_classification 6 7 18")
+ori_las <- readLAS("D:\\PROJECT_TREES\\REQ201107_Lars\\laser\\CL2_BE33_2021_1000_1019.las", filter="-drop_classification 6 7 18 -drop_z_below 0.1")
 
 las <- classify_noise(ori_las, sor(15,7))
 las_denoise <- filter_poi(las, Classification != LASNOISE)
@@ -14,28 +14,29 @@ las_denoise <- filter_poi(las, Classification != LASNOISE)
 n_las <- normalize_height(las_denoise, knnidw())
 
 getWS <- function(x){
-  y <- 2.6125 + x^0.665
+  y <- 1.6125 + x^0.665
   y[x<2] <- 2
-  y[x>40] <- 15
   return(y)
 }
 
-chm <- rasterize_canopy(n_las, 0.3, pitfree(subcircle = 0.2))
+chm <- rasterize_canopy(n_las, 1, pitfree(subcircle = 0.2))
 
 # to smooth the chm
 fill.na <- function(x, i=5) { if (is.na(x)[i]) { return(mean(x, na.rm = TRUE)) } else { return(x[i]) }}
 w <- matrix(1, 3, 3)
 
 smooth_chm <- terra::focal(chm, w, fun = mean, na.rm = TRUE)
-tree_tops <- locate_trees(n_las, lmf(ws = getWS, hmin = 18))
+tree_tops <- locate_trees(n_las, lmf(ws = getWS, hmin = 20))
 tree_tops2d <- sf::st_zm(tree_tops)
 plot(smooth_chm, col = col)
 plot(sf::st_geometry(tree_tops), add = TRUE, pch = 3)
 tree <- segment_trees(n_las, silva2016(smooth_chm, tree_tops, max_cr_factor = 0.3, exclusion = 0.25, ID="treeID"))
+#tree <- segment_trees(n_las, dalponte2016(smooth_chm, tree_tops, th_tree = 20 , max_cr = 20, ID="treeID"))
 
-#tree <- segment_trees(n_las, li2012(dt1 = 1.5, dt2 = 2, R = 2, Zu = 30, hmin = 20))
+#tree <- segment_trees(n_las, li2012(dt1 = 4, dt2 = 8, R = 0, Zu = 40, hmin = 20, speed_up = 8))
 crowns <- crown_metrics(  tree,  type = "convex",  func = .stdtreemetrics, attribute = "treeID", geom = "convex")
 
-st_write(crowns, "D:\\PROJECT_TREES\\isotree_trail\\crowns_0504.shp", append=FALSE)
-
+st_write(crowns, "D:\\PROJECT_TREES\\isotree_trail\\crowns_1019_silva_20plus.shp", append=FALSE)
+writeLAS(tree, "D:\\PROJECT_TREES\\isotree_trail\\trees_1019_silva_20plus.las")
+plot(crowns['Z'], add=TRUE)
 
