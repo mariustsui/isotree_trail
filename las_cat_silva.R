02a0848a8524690fd3dc2ec956256e71038c170c
Marius Tsui <kt255@students.waikato.ac.nz>
2023-10-20 08:43:39 +1300

    changed to catalog_apply for better capability to a single large LAS file

--- las_cat_silva.R
+++ las_cat_silva.R
@@ -18,10 +18,13 @@ rownames(forest_metrics) <- c("1M", "1.5M", "2M", "5M", "10M", "15M", "20M", "25
 
 # read all the point cloud (.las) into a lasCatalog
   
-
-  las_catg <- readLAScatalog("D:\\PROJECT_TREES\\REQ201107_Lars\\laser\\", filter="-drop_classification 6 7 18")
+  las_catg <- readLAScatalog("D:\\PROJECT_TREES\\MAUNGATAUTARI\\laser\\", filter="-drop_classification 6 7 18")
+  
+  #las_catg <- readLAScatalog("D:\\PROJECT_TREES\\PIRONGIA\\laser\\", filter="-drop_classification 6 7 18")
   #las_catg <- readLAScatalog("D:\\PROJECT_TREES\\LAS\\", filter="-drop_classification 6 7 18")
   
+  opt_chunk_buffer(las_catg) <- 30
+  opt_chunk_size(las_catg)   <- 1000      
   
   las_catg
   las_check(las_catg)
@@ -38,12 +41,23 @@ rownames(forest_metrics) <- c("1M", "1.5M", "2M", "5M", "10M", "15M", "20M", "25
   fill.na <- function(x, i=5) { if (is.na(x)[i]) { return(mean(x, na.rm = TRUE)) } else { return(x[i]) }}
   w <- matrix(1, 3, 3)
   
-  f_treecrown <- function(las, hmin){
+  f_treecrown <- function(chunk, hmin){
+    
+    # The chunk argument is a LAScluster object. The users do not need to know how it works.
+    # readLAS will load the region of interest (chunk) with a buffer around it, taking advantage of
+    # point cloud indexation if possible. The filter and select options are propagated automatically
+    las <- readLAS(chunk)
+    if (is.empty(las)) return(NULL)
     
     las <- classify_noise(las, sor(15,7))
     las_denoise <- filter_poi(las, Classification != LASNOISE)
     nlas <- normalize_height(las_denoise, knnidw())
     tree_tops <- locate_trees(nlas, lmf(ws = getWS, hmin = hmin))
+    
+    bbox <- st_bbox(chunk)
+    tree_tops <- sf::st_crop(tree_tops, bbox)
+    
+    
     chm <- rasterize_canopy(nlas, 1, pitfree(subcircle = 0.2))
     smooth_chm <- terra::focal(chm, w, fun = mean, na.rm = TRUE)
     tree <- segment_trees(nlas, silva2016(smooth_chm, tree_tops, max_cr_factor = 0.3, exclusion = 0.25, ID="treeID"))
@@ -57,9 +71,10 @@ rownames(forest_metrics) <- c("1M", "1.5M", "2M", "5M", "10M", "15M", "20M", "25
   print(paste0("Project started at ", start))
   
   loop_index <- loop_index + 1
-  opt    <- list(automerge = TRUE)   # catalog_apply will merge the outputs into a single object
+  opt    <- list(need_buffer = TRUE, automerge = TRUE)   # catalog_apply will merge the outputs into a single object
+  
   output = NULL
-  output <- catalog_map(las_catg, f_treecrown, hmin = hmin, .options = opt)
+  output <- catalog_apply(las_catg, f_treecrown, hmin = hmin, .options = opt)
   
   single_sf <- dplyr::bind_rows(output)
   types <- st_geometry_type(single_sf)
@@ -68,7 +83,7 @@ rownames(forest_metrics) <- c("1M", "1.5M", "2M", "5M", "10M", "15M", "20M", "25
   my_filtered_sf_object <- my_labelled_sf_object[my_labelled_sf_object$types == "POLYGON",]
   
   
-  st_write(my_filtered_sf_object, paste0("D:\\PROJECT_TREES\\R_OUTPUT\\CROWNS",'_',hmin,'PLUS_SILVA.shp' ), append=FALSE)
+  st_write(my_filtered_sf_object, paste0("D:\\PROJECT_TREES\\MAUNGATAUTARI\\R_OUTPUT\\CROWNS",'_',hmin,'PLUS_SILVA.shp' ), append=FALSE)
   #st_write(my_filtered_sf_object, paste0("D:\\PROJECT_TREES\\isotree_trail\\ALLCROWNS",'_',hmin,'PLUS_SILVA.shp' ), append=FALSE)
   
   
@@ -87,15 +102,15 @@ rownames(forest_metrics) <- c("1M", "1.5M", "2M", "5M", "10M", "15M", "20M", "25
     df[i, ] <- c(nrow(tree), round(nrow(tree)/ as.numeric(a_ha), 2), round(sum(tree$convhull_area), 2), round( (sum(tree$convhull_area)/as.numeric(a_m2)*100), 2))  
     ttop <- gCentroid( spgeom = methods::as( object = tree, Class = "Spatial" ) , byid = TRUE )
     
-    st_write(tree, paste0("D:\\PROJECT_TREES\\R_OUTPUT\\CROWNS\\CROWNS",'_',h*10,'CM_PLUS_SILVA.shp' ), append=FALSE)
+    st_write(tree, paste0("D:\\PROJECT_TREES\\MAUNGATAUTARI\\R_OUTPUT\\CROWNS\\CROWNS",'_',h*100,'CM_PLUS_SILVA.shp' ), append=FALSE)
     #st_write(tree, paste0("D:\\PROJECT_TREES\\isotree_trail\\CROWNS",'_',h,'PLUS_SILVA.shp' ), append=FALSE)    
-    st_write(st_as_sf(ttop), paste0("D:\\PROJECT_TREES\\R_OUTPUT\\TREETOPS\\TTOPS",'_',h*10,'CM_PLUS_SILVA.shp' ), append=FALSE)
+    st_write(st_as_sf(ttop), paste0("D:\\PROJECT_TREES\\MAUNGATAUTARI\\R_OUTPUT\\TREETOPS\\TTOPS",'_',h*100,'CM_PLUS_SILVA.shp' ), append=FALSE)
     #st_write(st_as_sf(ttop), paste0("D:\\PROJECT_TREES\\isotree_trail\\TTOPS",'_',h,'PLUS_SILVA.shp' ), append=FALSE)
 
     
     i <- i+1
   }
   
-  write.csv(df, "D:\\PROJECT_TREES\\R_OUTPUT\\FOREST_METRICS.csv")
+  write.csv(df, "D:\\PROJECT_TREES\\MAUNGATAUTARI\\R_OUTPUT\\FOREST_METRICS.csv")
   #write.csv(df, "D:\\PROJECT_TREES\\isotree_trail\\FOREST_METRICS.csv")
   
