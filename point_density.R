766d5abbaf8a307424fcb9744f7385a535a49305
Marius Tsui <kt255@students.waikato.ac.nz>
2023-10-30 11:43:01 +1300

    Added with point density raster at different level

--- point_density.R
+++ point_density.R
@@ -8,8 +8,8 @@ las_catg <- readLAScatalog("D:\\PROJECT_TREES\\MAUNGATAUTARI\\laser\\", filter="
 #las_catg <- readLAScatalog("D:\\PROJECT_TREES\\LAS\\", filter="-drop_classification 6 7 18")
 
 
-hMin = 2
-hMax = 5
+hMin = 30
+hMax = 30
 
 opt_chunk_buffer(las_catg) <- 30
 opt_chunk_size(las_catg)   <- 1000  
@@ -21,13 +21,12 @@ f_pointdensity <- function(chunk, hmin, hmax){
   las <- classify_noise(las, sor(15,7))
   las_denoise <- filter_poi(las, Classification != LASNOISE)
   nlas <- normalize_height(las_denoise, knnidw())
-  filtered_las = filter_poi(nlas, Z>=hmin, Z<=hmax)
+  filtered_las = filter_poi(nlas, Z>=hmin)
   pt_density = rasterize_density(filtered_las, 5)
   return(pt_density)
 }
 
 opt    <- list(need_buffer = TRUE, automerge = TRUE)   # catalog_apply will merge the outputs into a single object
 pt_den <- catalog_apply(las_catg, f_pointdensity, hmin = hMin, hmax = hMax, .options = opt)  
-writeRaster(pt_den, paste0("D:\\PROJECT_TREES\\MAUNGATAUTARI\\R_OUTPUT\\DENSITYMAP\\DENSITY",'_',hMin*100,'CM_', hMax*100,'CM.tif' ), overwrite=TRUE)
-
+writeRaster(pt_den, paste0("D:\\PROJECT_TREES\\MAUNGATAUTARI\\R_OUTPUT\\DENSITYMAP\\DENSITY",'_',hMin*100,'CM_PLUS.tif' ), overwrite=TRUE)
 
